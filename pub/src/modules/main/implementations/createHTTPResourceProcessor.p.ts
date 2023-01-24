c019971b3ea39919f8070e8380786526c4e1f0cb
corno <corno.astn.a@schraverus.com>
2023-01-24 20:40:20 +0100

    .

--- pub/src/modules/main/implementations/createHTTPResourceProcessor.p.ts
+++ pub/src/modules/main/implementations/createHTTPResourceProcessor.p.ts
@@ -2,13 +2,53 @@ import * as pi from "pareto-core-internals"
 
 import * as api from "../api"
 
+import * as http from "http"
+import * as pl from "pareto-core-internals"
+import * as pth from "path"
 
-import { call } from "../native/call.p"
+import * as mcommon from "glo-pareto-common"
 
 export const $$: api.CcreateHTTPResourceProcessor = ($x, $d) => {
     return ($, $c) => {
         const onError = $d.onError
         let consumer: null | api.IStreamConsumer = null
+        function call(
+            $: {
+                readonly "hostname": string,
+                readonly "path": mcommon.TPath,
+            },
+            $i: {
+                onData: (data: string) => void,
+                onError: (e: api.THTTPError) => void,
+                onEnd: () => void
+            }
+        ): void {
+            const options = {
+                hostname: $.hostname,
+                //port: 443,
+                path: pth.join(...pl.flatten($.path)),
+                method: 'GET'
+            }
+        
+            const req = http.request(options, res => {
+                res.setEncoding('utf-8')
+                //console.log(`statusCode: ${res.statusCode}`)
+        
+                res.on('data', d => {
+                    $i.onData(d)
+                })
+                res.on('end', () => {
+                    $i.onEnd()
+                })
+            })
+        
+            req.on('error', error => {
+                console.error(`FIX HTTP ERROR HANDLING; ${error.message}`)
+                $i.onError(["unknown", error.message])
+            })
+        
+            req.end()
+        }
         return call(
             {
                 hostname: $x.hostName,
