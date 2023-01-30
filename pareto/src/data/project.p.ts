03685fd6354dd61be18a2fb511a2ae677dd6d6d7
corno <corno.astn.a@schraverus.com>
2023-01-30 21:39:44 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -13,20 +13,17 @@ export const $: mproject.TProject = {
     'description': "a pareto wrapper around the HTTP library of NodeJS",
     'license': "ISC",
 
-    'type': ['resource',  {
-        'devDependencies': d({
-            "@types/node": {},
-        }),
-    }],
-    'modules': d({
-        "main": {
-            'definition': api,
-
-        },
-    }),
-    'main': "main",
     'pubdependencies': d({
         "glo-pareto-common": {},
     }),
-    'testdependencies': d({}),
+    'type': ['resource', {
+        'definition': api,
+        'devDependencies': d({
+            "@types/node": {},
+        }),
+        'test': {
+            'dependencies': d({
+            }),
+        }
+    }],
 }
