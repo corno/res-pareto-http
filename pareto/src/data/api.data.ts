fadceb84f77cde1f1a71811e7c504f45bb58b927
corno <corno.astn.a@schraverus.com>
2023-02-17 15:28:31 +0100

    .

--- pareto/src/data/api.data.ts
+++ pareto/src/data/api.data.ts
@@ -8,10 +8,10 @@ import {
     interfaceReference,
     null_,
     method, dictionary, group, member, taggedUnion, types, func, data, type
-} from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands.p"
+} from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands"
 
 
-import { definitionReference, constructor, algorithm } from "lib-pareto-typescript-project/dist/submodules/moduleDefinition/shorthands.p"
+import { definitionReference, constructor, algorithm } from "lib-pareto-typescript-project/dist/submodules/moduleDefinition/shorthands"
 import * as mmoduleDefinition from "lib-pareto-typescript-project/dist/submodules/moduleDefinition"
 
 const d = pd.wrapRawDictionary
