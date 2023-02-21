efb6ef24cab79b3d77234d13006742117c89579d
corno <corno.astn.a@schraverus.com>
2023-02-21 22:47:06 +0100

    .

--- pub/src/implementations/parse.ts
+++
@@ -1,6 +0,0 @@
-import * as fs from "fs"
-
-fs.readdirSync("./scripts").forEach((child) => {
-    console.log(`########"${child}":`)
-    console.log(fs.readFileSync(`./scripts/${child}`, { encoding: "utf-8"}))
-})
